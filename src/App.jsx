dec27b1e3c25455cf8278e244456e46b808591b7
phanindrareddy2006 <2300032080@kluniversity.in>
2025-08-06 09:47:24 +0530

    V1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    <BrowserRouter basename="/ecommerce-practice">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
